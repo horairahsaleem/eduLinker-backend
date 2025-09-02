d3cdd89a3d4d0fd21b58c9a0a5fd820b97b79bb5
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-02 09:04:28 +0500

    small update + tested

--- Routes/other.js
+++ Routes/other.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { isAuthenticated } from "../Middlewares/auth.js";
+import { contact, courseRequest } from "../Controllers/otherController.js";
 
 
 const  router = express.Router()
+router.route('/contact').post(isAuthenticated,contact)
+router.route('/courserequest').post(isAuthenticated,courseRequest)
 
 
 
