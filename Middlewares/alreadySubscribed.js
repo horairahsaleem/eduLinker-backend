e0d16b00301601bc46583fff1cc72a6d362cba34
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-25 13:31:46 +0500

    refix

--- Middlewares/alreadySubscribed.js
+++
@@ -1,9 +0,0 @@
-// middlewares/blockIfSubscribed.js
-export const alreadySubscribed = (req, res, next) => {
-  if (req.user.subscription?.status === "active") {
-    return res
-      .status(403)
-      .json({ message: "You are already subscribed" });
-  }
-  next();
-};
