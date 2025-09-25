a15d7947035cb140069a56e76aa9eccb85b1ca7c
horairahsaleem <horairahsaleem7864@gmail.com>
2025-09-25 13:16:39 +0500

    Add alreadysubscription check middleware

--- Routes/paymentsRoutes.js
+++ Routes/paymentsRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { isAuthenticated } from "../Middlewares/auth.js";
+import {alreadySubscribed} from "../Middlewares/alreadySubscribed.js"
 import { buySubscription, cancelSubscription, getStripeKey, paymentVerification } from "../Controllers/paymentController.js";
 
 
 const router = express.Router();
 
 // buy subcription 
-router.route('/subscribe').get(isAuthenticated,buySubscription)
+router.route('/subscribe').get(isAuthenticated,alreadySubscribed,buySubscription)
 router.route('/paymentverification').post(isAuthenticated,paymentVerification)
 router.route('/getstripekey').get(isAuthenticated,getStripeKey)
 router.route('/subscribe/cancel').delete(isAuthenticated,cancelSubscription)
